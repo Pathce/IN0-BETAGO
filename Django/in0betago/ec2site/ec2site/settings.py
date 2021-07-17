20a2b9035174042cc14e90c2ce62eacf8d7b0a38
Pathce <63540284+Pathce@users.noreply.github.com>
2021-07-17 13:14:05 +0900

    Update settings.py
    시크릿 키 수정

--- Django/in0betago/ec2site/ec2site/settings.py
+++ Django/in0betago/ec2site/ec2site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kp*8ht=mw90*4b0*q_qgvz1ilw^8pv!4c=_vj(z@#9j_)!4$t#'
+SECRET_KEY = '시크릿 키'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
